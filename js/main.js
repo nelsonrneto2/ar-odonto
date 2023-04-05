310ba4fe0952b9770f931ec752a60c1a35449f76
Nelson <nrodriguesneto@gmail.com>
2023-04-05 12:44:43 -0300

    second commit

---
+++ js/main.js
@@ -0,0 +1,48 @@
+
+function toggleMenu() {
+  const toggleMenu = document.querySelector('.toggleMenu')
+  const navigation = document.querySelector('.navigation')
+
+  toggleMenu.classList.toggle("active")
+  navigation.classList.toggle("active")
+}
+
+//Toggle Contact From
+function toggleContact() {
+  const toggleContact = document.querySelector('.toggleContact')
+  const contactMenu = document.querySelector('.contactMenu')
+
+  toggleContact.classList.toggle("c-active")
+  contactMenu.classList.toggle("c-active")
+}
+
+//End Toggle Contact From
+
+//Swiper Slide
+
+const swiper = new Swiper(".mySwiper", {
+  slidesPerView: 1,
+  spaceBetween: 10,
+  autoplay: {
+    delay: 2500,
+    disableOnInteraction: false,
+  },
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+  breakpoints: {
+    640: {
+      slidesPerView: 1,
+      spaceBetween: 20,
+    },
+    768: {
+      slidesPerView: 2,
+      spaceBetween: 40,
+    },
+    1024: {
+      slidesPerView: 2,
+      spaceBetween: 50,
+    }
+  }
+});
